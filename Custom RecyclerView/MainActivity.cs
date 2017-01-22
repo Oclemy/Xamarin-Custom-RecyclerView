940ad4795eb9a035aee060d928fb13c8852e3d3b
Oclemy <oclemmi@gmail.com>
2017-01-22 04:48:37 -0800

    Initial Commit

---
+++ Custom RecyclerView/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Custom_RecyclerView.mData;
+using Custom_RecyclerView.mRecycler;
+
+
+namespace Custom_RecyclerView
+{
+    [Activity(Label = "Custom_RecyclerView", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private RecyclerView rv;
+        private MyAdapter adapter;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+
+            //RV
+            rv = FindViewById<RecyclerView>(Resource.Id.mRecylcerID);
+            rv.SetLayoutManager(new LinearLayoutManager(this));
+            rv.SetItemAnimator(new DefaultItemAnimator());
+
+            //ADAPTER
+            adapter=new MyAdapter(TVShowsCollection.getTVShows());
+            rv.SetAdapter(adapter);
+
+            adapter.ItemClick += onItemClick;
+        }
+
+        void onItemClick(object sender, int position)
+        {
+            Toast.MakeText(this,TVShowsCollection.getTVShows()[position].Name,ToastLength.Short).Show();
+        }
+
+    }
+}
+
